91f72f10dda4e38ca784c99bf97f7412a5d2077d
Fatema-tuj-Jahura <fatema.tuj.jahura@g.bracu.ac.bd>
2024-09-25 23:17:40 +0600

    Connected blog and home page

--- js/features.js
+++ js/features.js
@@ -26,5 +26,5 @@ document.getElementById('history-btn').addEventListener('mouseout', function () 
 
 document.getElementById('blog-btn')
   .addEventListener('click', function(){
-    window.location.href = '/blog.html';
+    window.location.href = './blog.html';
   }) 
